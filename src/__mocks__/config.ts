b5b0a7723d84c96c085d42a98530895d00711f8a
Keat Tang <keat@propelleraero.com.au>
2025-04-13 10:59:12 +1000

    Deleted tests

--- src/__mocks__/config.ts
+++
@@ -1,2 +0,0 @@
-export const GOOGLE_MAPS_API_KEY = 'mock-api-key';
-export const GOOGLE_MAPS_LIBRARIES = ['places']; 
--- src/__mocks__/vite.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  meta: {
-    env: {
-      VITE_GOOGLE_MAPS_API_KEY: 'mock-api-key',
-      VITE_GOOGLE_MAPS_LIBRARIES: 'places',
-      NODE_ENV: 'test',
-    },
-  },
-}; 
